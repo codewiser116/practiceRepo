d737efcdd693eecee1c75948bebdb73118d95e8f
codewiser116 <benazir.baialieva@gmail.com>
2024-03-14 19:00:54 -0500

    added initial files

---
+++ src/test/java/UnitTests.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class UnitTests {
+
+    @Test
+    public void loginTest(){
+        System.out.println("Login test!");
+    }
+
+    //5. push src/ pom.xml .gitignore to remote repo
+
+
+}
